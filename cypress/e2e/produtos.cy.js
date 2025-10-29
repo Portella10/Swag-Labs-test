985c6f835b0283a9567137407a291b9534ebbcf9
João Portella <{jvportella.m@gmail.com}>
2025-10-29 13:04:25 -0300

    Completei testes de produto onde valida todos os links dos produtos com suas respectivas paginas

--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -1,16 +1,26 @@
 /// <reference types="cypress" />
 
 describe("Produtos", () => {
+    const produtos = [
+        { nome: "Sauce Labs Backpack", seletor: "#item_4_title_link" },
+        { nome: "Sauce Labs Bike Light", seletor: "#item_0_title_link" },
+        { nome: "Sauce Labs Bolt T-Shirt", seletor: "#item_1_title_link" },
+        { nome: "Sauce Labs Fleece Jacket", seletor: "#item_5_title_link" },
+        { nome: "Sauce Labs Onesie", seletor: "#item_2_title_link" },
+        { nome: "Test.allTheThings() T-Shirt (Red)", seletor: "#item_3_title_link" },
+    ];
+
     beforeEach(() => {
         cy.visit("/");
         cy.get("#user-name").click().type("problem_user");
         cy.get("#password").click().type("secret_sauce");
         cy.get("#login-button").click();
     });
-
-    it("Ao clicar no link do produto deve ser redirecionado para a pagina do produto ", () => {
-        cy.get("#item_4_title_link").click();
-        cy.log("Bug, Link do produto nao corresponde ao da pagina");
-        cy.get(".inventory_details_name").should("have.text", "Sauce Labs Backpack");
+    produtos.forEach((produto) => {
+        it(`Verifica se o link do produto corresponde a sua pagina ${produto.nome}`, () => {
+            cy.get(produto.seletor).click();
+            cy.log(`Validando produto: ${produto.nome}`);
+            cy.get(".inventory_details_name").should("have.text", produto.nome);
+        });
     });
 });
